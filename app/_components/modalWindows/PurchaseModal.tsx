fff7ccf25e9a1b919289498e509d98fd87a6e4a7
Tyrus Berggren <tyrusbrianberggren@gmail.com>
2026-03-29 00:06:23 -0400

    update purchase links

--- app/_components/modalWindows/PurchaseModal.tsx
+++ app/_components/modalWindows/PurchaseModal.tsx
@@ -14,10 +14,16 @@ const PurchaseModal: FC<PurchaseModalProps> = ({}) => {
       </div>
 
       <div className={styles.buttons}>
-        <Button href="/#" btnStyle="cta">
+        <Button
+          href="https://www.amazon.com/Tanglefolk-Elle-Saoirse-Rogers/dp/B0GV1SHVPR/ref=sr_1_1?crid=1N92Y6L6XKC2W&dib=eyJ2IjoiMSJ9.swDdkZyhjph7DEELtgsYSQ.fZhE5gfdGzu3_pSgTlKnVVNSn60QAbiMQWTaDQYBceE&dib_tag=se&keywords=The+Tanglefolk&qid=1774723126&sprefix=the+tanglefolk%2Caps%2C201&sr=8-1"
+          btnStyle="cta"
+        >
           Amazon
         </Button>
-        <Button href="/#" btnStyle="cta">
+        <Button
+          href="https://www.barnesandnoble.com/w/the-tanglefolk-elle-saoirse-rogers/1149778921?ean=9798822985858"
+          btnStyle="cta"
+        >
           Barnes & Noble
         </Button>
       </div>
